5b060b6a83932865cc8ce807c57b4b6a63fb013f
iambashar <bashar.onion@gmail.com>
2021-10-12 00:42:16 +0600

    Backend setup done

---
+++ Server/server.js
@@ -0,0 +1,9 @@
+require("dotenv").config();
+const express = require("express");
+
+const app = express();
+
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+  console.log(`server is up and listening on port ${port}`);
+});
