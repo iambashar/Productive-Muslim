763a04585ed56e19b37433008dbbe9f5acf05d06
SamiaShashmi <shoshmiislam149@gmail.com>
2021-10-12 06:39:52 +0600

    react setup done

---
+++ Client/productive-muslim/src/App.js
@@ -0,0 +1,54 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import Homepage from './pages/Homepage/Homepage';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams
+} from "react-router-dom";
+import Dua from './pages/Dua/Dua';
+import NavBar from './components/NavBar/navBar';
+import Salah from './pages/Salah/Salah';
+import Sawm from './pages/Sawm/Sawm';
+import Tracker from './pages/Tracker/Tracker';
+import Challenges from './pages/Challenges/Challenges';
+import Forum from './pages/Forum/Forum';
+
+
+function App() {
+  return (
+    
+      <Router>
+        <Switch>
+          <Route path="/pages/Dua">
+            <Dua></Dua>
+          </Route>
+          <Route path="/pages/Salah">
+            <Salah></Salah>  
+          </Route>
+          <Route path="/pages/Sawm">
+            <Sawm></Sawm>
+          </Route>
+          <Route path="/pages/Tracker">
+            <Tracker></Tracker>
+          </Route>
+          <Route path="/pages/Challenges">
+            <Challenges></Challenges>
+          </Route>
+          <Route path="/pages/Forum">
+            <Forum></Forum>
+          </Route>
+          <Route path="/">
+            <Homepage></Homepage>
+          </Route>
+        </Switch>
+      </Router>
+  );
+}
+
+export default App;
---
+++ Client/productive-muslim/src/components/NavBar/navBar.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Navbar, Container, Nav } from 'react-bootstrap';
+import textIcon from '../../Images/textIcon.svg'
+import mainIcon from '../../Images/mainIcon.svg'
+
+const navBar = () => {
+    return (
+        <div>
+
+            <header class="navBar">
+                <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                    <Container>
+                        <Navbar.Brand href="#">
+                            <img src={mainIcon} alt="logo" width="50" />
+                            <img src={textIcon} alt="logo" width="200" />
+                        </Navbar.Brand>
+                    </Container>
+                    <Container>
+                        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                        <Navbar.Collapse id="responsive-navbar-nav">
+                            <Nav className="ms-auto">
+                                <Nav.Link href="../../pages/Homepage">Home</Nav.Link>
+                                <Nav.Link href="../../pages/Dua">Dua</Nav.Link>
+                                <Nav.Link href="../../pages/Salah">Salah</Nav.Link>
+                                <Nav.Link href="../../pages/Sawm">Sawm</Nav.Link>
+                                <Nav.Link href="../../pages/Tracker">Tracker</Nav.Link>
+                                <Nav.Link href="../../pages/Challenges">Challenges</Nav.Link>
+                                <Nav.Link href="../../pages/Forum">Forum</Nav.Link>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Container>
+                </Navbar>
+            </header>
+        </div>
+    );
+};
+
+export default navBar;
