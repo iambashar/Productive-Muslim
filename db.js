ac79307f1c2eb093a2a1c91c5527e37f18dc4a9e
iambashar <bashar.onion@gmail.com>
2021-11-07 08:46:36 +0600

    db.js changed

--- db.js
+++ db.js
@@ -1,4 +1,5 @@
 const Pool = require("pg").Pool;
+pg.defaults.ssl = true;
 require("dotenv").config();
 
 const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
