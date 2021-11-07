848d37e91ea0f3d6a54bcc3e5a2356d16a14abce
iambashar <bashar.onion@gmail.com>
2021-11-08 02:57:37 +0600

    updatepost mistake fixed

--- Client/src/components/Authentication/UpdateProfile.js
+++ Client/src/components/Authentication/UpdateProfile.js
@@ -78,7 +78,16 @@ export default function UpdateProfile() {
 
   return (
     <>
-      {currentUser.displayName == null ? <div className="annonimousdiv"><h1 className="annonimousalingment">Please login with an account to see the posts!<br/><br/>Jazakumullah Khair</h1> </div> :
+      {currentUser.displayName == null ?
+        <>
+          <div className="annonimousdiv"><h1 className="annonimousalingment">Please login with an account to see the posts!<br />
+            <br />Jazakumullah Khair</h1> </div>
+          <div className="btng" id="cancelbtn">
+            <Button className="w-100" id="btn" onClick={() => history.push("/")}>
+              Cancel
+            </Button>
+          </div>
+        </> :
         <Card className="emotionBox2">
           <Card.Body>
             <h1 className="text-center mb-4">Update Profile</h1>
@@ -138,11 +147,6 @@ export default function UpdateProfile() {
           </Card.Body>
         </Card>
       };
-          <div className="btng" id="cancelbtn">
-            <Button className="w-100" id="btn" onClick={()=>history.push("/")}>
-              Cancel
-            </Button>
-          </div>
     </>
   )
 }
